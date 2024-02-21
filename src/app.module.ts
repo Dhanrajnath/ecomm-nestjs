16f0866e104b8bd2f27d5e0a2ef20b0fbb3be625
dhanrajnath.porika <dhanrajnath.porika@zemosolabs.com>
2024-02-21 11:08:16 +0530

    configured ConfigModule & Validation, TypeORM postgreSQL conn

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${process.env.STAGE}`],
+      isGlobal: true,
+      validationSchema: configValidationSchema
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true
+      })
+    })
+  ],
   controllers: [],
-  providers: [],
+  providers: [ConfigService],
 })
 export class AppModule { }
