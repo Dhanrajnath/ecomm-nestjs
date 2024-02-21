e63b56e8915cbb04ba825696b901b41ceeb60da4
dhanrajnath.porika <dhanrajnath.porika@zemosolabs.com>
2024-02-21 10:51:50 +0530

    configured swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce App')
+    .setDescription('The E-comm app API description')
+    .setVersion('1.0')
+    .addTag('ecomm')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 
   if (module.hot) {
